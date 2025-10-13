fb89f5fe2ce77504d4ba8385f156e8c993ed51c2
Diego Sanchez Franco <diego.sanchez@ivaq.es>
2025-10-13 15:39:19 +0200

    changed to bulk

--- samples/sample_c/platform/linux/manifold2/application/dji_sdk_config.h
+++ samples/sample_c/platform/linux/manifold2/application/dji_sdk_config.h
@@ -30,20 +30,21 @@
 /* Includes ------------------------------------------------------------------*/
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 /* Exported constants --------------------------------------------------------*/
-#define DJI_USE_ONLY_UART                  (0)
-#define DJI_USE_UART_AND_USB_BULK_DEVICE   (1)
-#define DJI_USE_UART_AND_NETWORK_DEVICE    (2)
+#define DJI_USE_ONLY_UART (0)
+#define DJI_USE_UART_AND_USB_BULK_DEVICE (1)
+#define DJI_USE_UART_AND_NETWORK_DEVICE (2)
 
 /*!< Attention: Select your hardware connection mode here.
-* */
-#define CONFIG_HARDWARE_CONNECTION         DJI_USE_UART_AND_NETWORK_DEVICE
+ * */
+#define CONFIG_HARDWARE_CONNECTION DJI_USE_UART_AND_USB_BULK_DEVICE
 
 /*!< Attention: Select the sample you want to run here.
-* */
+ * */
 #define CONFIG_MODULE_SAMPLE_POWER_MANAGEMENT_ON
 
 #define CONFIG_MODULE_SAMPLE_CAMERA_EMU_ON
@@ -66,13 +67,13 @@ extern "C" {
 
 #define CONFIG_MODULE_SAMPLE_HMS_CUSTOMIZATION_ON
 
-/*!< Attention: This function needs to be used together with mobile sdk mop sample.
-* */
-//#define CONFIG_MODULE_SAMPLE_MOP_CHANNEL_ON
+    /*!< Attention: This function needs to be used together with mobile sdk mop sample.
+     * */
+    // #define CONFIG_MODULE_SAMPLE_MOP_CHANNEL_ON
 
-/* Exported types ------------------------------------------------------------*/
+    /* Exported types ------------------------------------------------------------*/
 
-/* Exported functions --------------------------------------------------------*/
+    /* Exported functions --------------------------------------------------------*/
 
 #ifdef __cplusplus
 }
