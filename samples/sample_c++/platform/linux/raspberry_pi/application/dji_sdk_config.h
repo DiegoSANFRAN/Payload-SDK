a46a602c1a1a22e53387b324cbf45ec761e22717
Diego Sanchez Franco <diego.sanchez@ivaq.es>
2025-10-14 21:11:57 +0200

    set samples to false

--- samples/sample_c++/platform/linux/raspberry_pi/application/dji_sdk_config.h
+++ samples/sample_c++/platform/linux/raspberry_pi/application/dji_sdk_config.h
@@ -48,15 +48,15 @@ extern "C" {
 
 #define CONFIG_MODULE_SAMPLE_CAMERA_MEDIA_ON            true
 
-/* #define CONFIG_MODULE_SAMPLE_GIMBAL_EMU_ON              true
+#define CONFIG_MODULE_SAMPLE_GIMBAL_EMU_ON              false
 
-#define CONFIG_MODULE_SAMPLE_WIDGET_ON                  true
+#define CONFIG_MODULE_SAMPLE_WIDGET_ON                  false
 
-#define CONFIG_MODULE_SAMPLE_WIDGET_SPEAKER_ON          true
+#define CONFIG_MODULE_SAMPLE_WIDGET_SPEAKER_ON          false
 
-#define CONFIG_MODULE_SAMPLE_POWER_MANAGEMENT_ON        true
+#define CONFIG_MODULE_SAMPLE_POWER_MANAGEMENT_ON        false
 
-#define CONFIG_MODULE_SAMPLE_DATA_TRANSMISSION_ON       true */
+#define CONFIG_MODULE_SAMPLE_DATA_TRANSMISSION_ON       false
 
 /* Exported types ------------------------------------------------------------*/
 
